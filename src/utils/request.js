913a107086d1a5c2881c219ed55d081798680472
LiuYinghao <liuyinghao4618@163.com>
2022-06-27 22:37:24 +0800

    fix(git): token and token

---
+++ src/utils/request.js
@@ -0,0 +1,83 @@
+/**
+ * 引入axios
+ * */
+import axios from 'axios'
+
+import md5 from 'md5'
+
+import loading from './loading'
+
+/**
+ * 创建axio s实例对象
+ * */
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
+
+/**
+ *    请求拦截器
+ */
+service.interceptors.request.use(
+  (config) => {
+    loading.open()
+    const { icode, time } = getTestICode()
+    config.headers.icode = icode
+    config.headers.codeType = time
+
+    return config
+  },
+  (error) => {
+    loading.close()
+    return Promise.reject(error)
+  }
+)
+
+/**
+ *    响应拦截器
+ */
+service.interceptors.response.use(
+  (response) => {
+    // 关闭loading加载
+    loading.close()
+
+    // TODO token过期状态
+
+    // TODO 全局响应处理
+
+    return response
+  },
+  (error) => {
+    // 关闭loading加载
+    loading.close()
+    return Promise.reject(error)
+  }
+)
+
+/**
+ * 统一传参处理
+ * */
+
+const request = (options) => {
+  if (options.method.toLowerCase() === 'get') {
+    options.params = options.data || {}
+  }
+  return service(options)
+}
+
+/**
+ * 实现code
+ *  */
+function getTestICode() {
+  const now = parseInt(Date.now() / 1000)
+  const code = now + 'LGD_Sunday-1991'
+  return {
+    icode: md5(code),
+    time: now
+  }
+}
+
+/**
+ * \导出axios实例对象
+ *  */
+export default request
