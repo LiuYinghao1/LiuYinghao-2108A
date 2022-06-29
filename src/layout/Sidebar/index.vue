3cb773272cd904bcc374dfde47e788266b0be06b
LiuYinghao <liuyinghao4618@163.com>
2022-06-29 21:23:57 +0800

    feat(router): router 配置完成,跳转成功

--- src/layout/Sidebar/index.vue
+++ src/layout/Sidebar/index.vue
@@ -1,74 +1,19 @@
 <template>
-  <div class="wapper">
-    <ment-tree :menu="menuList"></ment-tree>
+  <div>
+    <h1 class="sidebar-title">usian-admin</h1>
+    <sidebar-menu></sidebar-menu>
   </div>
 </template>
+
 <script setup>
-import { reactive } from 'vue'
-import MentTree from './MentTree'
-const menuList = reactive([
-  {
-    title: '个人中心',
-    path: '/profile'
-  },
-  {
-    title: '用户',
-    children: [
-      {
-        title: '员工管理',
-        path: '/staff',
-        children: [
-          {
-            title: '市场员工'
-          },
-          {
-            title: '研发员工'
-          }
-        ]
-      },
-      {
-        title: '角色列表',
-        path: '/role',
-        children: [
-          {
-            title: '用户角色'
-          },
-          {
-            title: '员工角色'
-          }
-        ]
-      },
-      {
-        title: '权限列表',
-        path: '/premisson'
-      }
-    ]
-  },
-  {
-    title: '文章',
-    children: [
-      {
-        title: '文章排名',
-        path: '/article'
-      },
-      {
-        title: '创建文章',
-        path: '/create'
-      }
-    ]
-  }
-])
+import SidebarMenu from './SidebarMenu'
 </script>
+
 <style lang="scss" scoped>
-// .wapper {
-//   background-color: #304156;
-//   width: 210px;
-//   height: 100%;
-// }
-p {
-  text-align: center;
-  margin-bottom: 30px;
-  color: #fff;
-  font-size: 24px;
+.sidebar-title{
+  text-align : center;
+  padding : 15px 0;
+  font-size : 22px;
+  color : #fff;
 }
 </style>
